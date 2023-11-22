dc8cd6bbcceb5d209a297be7b2a1329ba1f38556
Obrunow <brunocesarm31@gmail.com>
2023-11-22 17:25:42 -0300

    alteração na logica

--- scrip/script.js
+++ scrip/script.js
@@ -4,6 +4,8 @@ const mostrarResultado = document.querySelector("[data-texto]");
 const mostrarResultado1 = document.querySelector("[data-texto1]")
 const mostrarVencedor = document.querySelector(".vencedor");
 const jogarDnv = document.querySelector("[data-jogarDnv]");
+const atual = document.querySelector(".atual")
+const mostrarVez = document.querySelector(".mostrarVez")
 
 let eVezDoCirculo = false;
 
@@ -13,23 +15,25 @@ const combinacoesDeVitoria = [
   [0, 4, 8], [2, 4, 6]
 ];
 
-function começarJogo(){
+function começarJogo() {
+  mostrarVez.style.display = "flex"
+  atual.innerHTML = eVezDoCirculo == true? "x" : "bola";
   jogadadoDoJogo.forEach(quadrado => {
-    quadrado.addEventListener("click", clickar, {once:true} );
+    quadrado.addEventListener("click", clickar, { once: true });
   });
 }
 
-function restartar(){
-    jogadadoDoJogo.forEach(jogada =>{
-        jogada.classList.remove("x", "bola");
-        jogada.innerHTML = ""
-        começarJogo()
-    });
+function restartar() {
+  jogadadoDoJogo.forEach(jogada => {
+    jogada.classList.remove("x", "bola");
+    jogada.innerHTML = ""
+    começarJogo()
+  });
 
-    eVezDoCirculo =false;
-    mostrarResultado.classList.add("final");
-    mostrarVencedor.classList.remove("bola", "x");
-    mostrarVencedor.innerHTML = "";
+  eVezDoCirculo = false;
+  mostrarResultado.classList.add("final");
+  mostrarVencedor.classList.remove("bola", "x");
+  mostrarVencedor.innerHTML = "";
 }
 
 const checarPorVitoria = (jogadorAtual) => {
@@ -59,7 +63,7 @@ function x() {
   if (!this.classList.contains("bola")) {
     this.innerHTML = "X";
     this.classList.add("x");
-    
+
   }
 }
 
@@ -77,21 +81,29 @@ const trocaTurno = () => {
 function clickar() {
   const vezAtual = trocaTurno();
   vezAtual.call(this);
-
+  atual.innerHTML = eVezDoCirculo == true? "x" : "bola";
   const jogadorAtual = eVezDoCirculo ? "bola" : "x";
   const vencedor = checarPorVitoria(jogadorAtual);
   const empate = checarEmpate();
 
   if (vencedor) {
+    mostrarVez.style.display = "none"
     console.log("Vencedor: ", jogadorAtual);
+    jogadadoDoJogo.forEach(quadrado =>{
+      quadrado.removeEventListener("click", clickar)
+    })
     mostrarResultado.classList.remove("final");
     mostrarVencedor.classList.add(jogadorAtual);
     mostrarVencedor.innerHTML = jogadorAtual;
+    atual.innerHTML= "bola"
   } else if (empate) {
+    mostrarVez.style.display = "none"
     console.log("Empate");
     mostrarResultado.classList.remove("final");
     mostrarResultado1.innerHTML = "Deu velha";
+    atual.innerHTML= "bola"
   }
+  
 }
 
 jogarDnv.addEventListener("click", restartar);
